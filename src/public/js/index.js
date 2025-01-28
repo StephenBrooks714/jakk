f357dbe21db9dd67d0cf70740127db0a32661fda
StephenBrooks714 <stephenbrooks714@gmail.com>
2025-01-27 20:46:42 -0600

    update to layout

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -2,10 +2,10 @@
 window.addEventListener('scroll', () => {
     const navbar = document.getElementById('navbar')
     if (window.scrollY > 0) {
-        navbar.classList.add('bg-dark')
+        navbar.classList.add('bg-green-nav-shadow')
         navbar.classList.add('fadeInDown')
     } else {
-        navbar.classList.remove('bg-dark')
+        navbar.classList.remove('bg-green-nav-shadow')
         navbar.classList.remove('fadeInDown')
     }
 })
